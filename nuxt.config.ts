c2c5c24a681b1d3a31ae30549317ff69d6e9677b
Jason Theiler <turret.jason@outlook.com>
2020-09-12 16:14:20 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,91 @@
+import { NuxtConfig } from "@nuxt/types"
+
+const config: NuxtConfig = {
+  /*
+   * Nuxt rendering mode
+   * See: https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+
+  /*
+   * Nuxt target
+   * See: https://nuxtjs.org/api/configuration-target
+   */
+  target: "static",
+
+  /*
+   * Nuxt source directory
+   * See: https://nuxtjs.org/api/configuration-srcdir
+   */
+  srcDir: "src",
+
+  /*
+   * Headers of the page
+   * See: https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    titleTemplate: (title) =>
+      title ? `${title} | Jason Theiler` : "Jason Theiler",
+    meta: [
+      { charset: "utf-8" },
+      {
+        hid: "viewport",
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Work in progress!",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  /*
+   * Global CSS
+   */
+  css: [],
+
+  /*
+   * Plugins to load before mounting the app
+   * See: https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+
+  /*
+   * Auto import components
+   * See: https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+
+  /*
+   * Nuxt.js dev-modules
+   * See:
+   *   - https://typescript.nuxtjs.org/
+   *   - https://tailwindcss.nuxtjs.org/
+   */
+  buildModules: ["@nuxt/typescript-build", "@nuxtjs/tailwindcss"],
+
+  /*
+   * Nuxt.js modules
+   * See:
+   *   - https://pwa.nuxtjs.org/
+   *   - https://content.nuxtjs.org/
+   */
+  modules: ["@nuxtjs/pwa", "@nuxt/content"],
+
+  /*
+   * Content module configuration
+   * See: https://content.nuxtjs.org/configuration
+   */
+  content: {},
+
+  /*
+   * Build configuration
+   * See: https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+}
+
+export default config
