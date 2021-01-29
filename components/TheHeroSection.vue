0844b1ff6a46250f278862e6b97febbdf5fc56f7
Jason Theiler <turret.jason@outlook.com>
2021-01-29 05:05:57 +0100

    Add basic cloud particles

--- components/TheHeroSection.vue
+++ components/TheHeroSection.vue
@@ -1,7 +1,9 @@
 <template>
   <section
-    class="w-full -mt-36 mb-16 pt-36 pb-16 bg-gray-50 dark:bg-gray-1100 transition-colors duration-150"
+    class="relative w-full -mt-36 mb-16 pt-36 pb-16 bg-gray-50 dark:bg-gray-1100 transition-colors duration-150"
   >
+    <Clouds id="clouds-bg" class="absolute inset-0" />
+
     <div
       class="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col lg:flex-row"
     >
