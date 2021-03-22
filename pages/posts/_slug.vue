b2813349900f91653d85f22f48e23a8a6b1f2bbd
Jason Theiler <turret.jason@outlook.com>
2021-03-22 09:36:11 +0100

    Add arrow animations to post navigation elements

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -44,7 +44,7 @@
             >
               <svg
                 viewBox="0 0 20 20"
-                class="flex-shrink-0 w-6 h-6 mr-2 fill-current"
+                class="flex-shrink-0 w-6 h-6 mr-2 transform-gpu group-hover:-translate-x-1 fill-current transition-transform duration-150"
               >
                 <path
                   d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
@@ -75,7 +75,7 @@
 
               <svg
                 viewBox="0 0 20 20"
-                class="flex-shrink-0 w-6 h-6 ml-2 fill-current"
+                class="flex-shrink-0 w-6 h-6 ml-2 transform-gpu group-hover:translate-x-1 fill-current transition-transform duration-150"
               >
                 <path
                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
